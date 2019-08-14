f988fcec4b3a9a2f6bca9e0b73c3d0664db9e087
xgironx <xgironx@gmail.com>
2019-08-13 22:09:52 -0400

    my first py repo

---
+++ action/super-pki-wizard.py
@@ -0,0 +1,5 @@
+path = '/Users/justingiron/OneDrive - BOOZ ALLEN HAMILTON/x/bah/py2019/code_script/python/super-pki-wizard/in/hostinfo.txt.array'
+file_pki = open(path,'r')
+file_pki_read = file_pki.read()
+print(file_pki_read)
+file_pki.close()
